d889b307a616a86b45b3a5d2dbee065f041c2085
Llewellyn Falco <isidore@setgame.com>
2016-05-24 04:06:40 -0700

    moved bowling to own file

---
+++ ExtremeFakeItTillYouMakeIt/Bowling.cs
@@ -0,0 +1,25 @@
+﻿namespace ExtremeFakeItTillYouMakeIt
+{
+    public class Bowling
+    {
+        public void Roll(params int[] rolls)
+        {
+        }
+
+        public override string ToString()
+        {
+            var expected = @"
+1) 3, 5 [8] = 8
+2) 10 [20] = 28
+3) 3, 7 [18] = 46
+4) 8, 1 [9] = 55
+5) 10 [26] = 81
+6) 10 [18] = 99
+7) 6, 2 [8] = 107
+8) 5, 4 [9] = 116
+9) 7, 3 [20] = 136
+10) 10, 6, 3 [19] = 155".TrimStart();   
+            return expected;
+        }
+    }
+}
